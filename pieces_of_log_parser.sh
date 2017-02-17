0a0ff56b60a3175d5a50707f1b7355740518dce3
CommanderAsdasd <e1ectricwiz4rd@gmail.com>
2017-02-17 12:21:30 +0300

    Update pieces_of_log_parser.sh

--- pieces_of_log_parser.sh
+++ pieces_of_log_parser.sh
@@ -1,7 +1,7 @@
 # Поиск фишеров: "moura rodrigo Silva Carlos angelica Esanu Gean Ortega Castaneda Endio mario Pedro soares rafael silva coelho Julia amaral costa Xing Ling Fahad Niazi Francisco oliveira marcos Felipe Ana Luiza Magalhaes REZENDE DOWSON daniel hassan usama Ремизов
 # Последние сервера на которые регистрация
 `
-{ logs n vh48 & logs n vh50 & logs n bitrix152; } | egrep "americanas|banking|Canada|blackfriday|visa|cheap|jackson|flight|ofertas|carnaval|walmart|cadastro|garantia|Galaxy|smartphone" > temp.txt; tac temp.txt | egrep -v " 404 | 403 | 500 | 503 |*error_domain.*" | awk '{print $6}' | egrep "[a-zA-Z]{2}[0-9]{5}" | sort | uniq > temp`date +%H-%M`.txt
+{ logs n vh48 & logs n vh50 & logs n bitrix152; } | egrep "americanas|banking|Canada|blackfriday|visa|cheap|jackson|flight|ofertas|carnaval|walmart|cadastro|garantia|Galaxy|smartphone" > temp.txt; tac temp.txt | egrep -v " 404 | 403 | 500 | 503 |*error_domain.*" | awk '{print $6}' | egrep -o "[a-zA-Z]{2}[0-9]{5}" | sort | uniq > temp`date +%H-%M`.txt
 
 #/bin/bash
 chromium-browser "https://staff2.timeweb.ru/oldstaff/load?module=mod_customers&task=find&critery=cust_login&findvalue=$1&ctype=all"
